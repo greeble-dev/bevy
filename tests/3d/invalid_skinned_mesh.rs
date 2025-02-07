961e1c0d07182f798e0542e1fcc8c40dc4eed699
Greeble <166992735+greeble-dev@users.noreply.github.com>
2025-02-07 00:10:13 +0000

    How about we exercise all the modes?

--- tests/3d/invalid_skinned_mesh.rs
+++ tests/3d/invalid_skinned_mesh.rs
@@ -4,8 +4,9 @@
 //! mesh should be animating. The middle and right meshes should be static.
 
 use bevy::{
-    core_pipeline::prepass::DepthPrepass,
+    core_pipeline::prepass::{DeferredPrepass, DepthPrepass, MotionVectorPrepass, NormalPrepass},
     math::ops,
+    pbr::DefaultOpaqueRendererMethod,
     prelude::*,
     render::{
         mesh::{
@@ -14,13 +15,17 @@ use bevy::{
         },
         render_asset::RenderAssetUsages,
     },
+    time::common_conditions::on_timer,
 };
+use core::time::Duration;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(DefaultOpaqueRendererMethod::forward())
         .add_systems(Startup, setup)
         .add_systems(Update, joint_animation)
+        .add_systems(Update, switch_mode.run_if(on_timer(Duration::from_secs(2))))
         .run();
 }
 
@@ -36,8 +41,8 @@ fn setup(
     commands.spawn((
         Camera3d::default(),
         Transform::from_xyz(0.0, 0.0, 4.0).looking_at(Vec3::ZERO, Vec3::Y),
-        // Make sure we exercise the renderer's prepass path.
-        DepthPrepass,
+        // MSAA is incompatible with deferred rendering.
+        Msaa::Off,
     ));
 
     // Add a directional light to make sure we exercise the renderer's lighting path.
@@ -61,6 +66,15 @@ fn setup(
                 [0.5, -0.5, 0.0],
                 [-0.5, 0.5, 0.0],
                 [0.5, 0.5, 0.0],
+            ],
+        )
+        .with_inserted_attribute(
+            Mesh::ATTRIBUTE_NORMAL,
+            vec![
+                [0.0, 0.0, 1.0],
+                [0.0, 0.0, 1.0],
+                [0.0, 0.0, 1.0],
+                [0.0, 0.0, 1.0],
             ],
         )
         .with_inserted_attribute(
@@ -133,5 +147,63 @@ fn setup(
 fn joint_animation(time: Res<Time>, mut query: Query<(&mut Transform, &AnimatedJoint)>) {
     for (mut transform, _) in &mut query {
         transform.rotation = Quat::from_rotation_y(ops::sin(time.elapsed_secs()));
+    }
+}
+
+#[derive(Resource, Default, Debug, Copy, Clone)]
+enum RenderMode {
+    #[default]
+    Forward,
+    ForwardPrepass,
+    Deferred,
+}
+
+fn switch_mode(
+    mut commands: Commands,
+    mut default_opaque_renderer_method: ResMut<DefaultOpaqueRendererMethod>,
+    cameras: Query<Entity, With<Camera>>,
+    mut mode: Local<RenderMode>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let previous_mode = *mode;
+
+    *mode = match *mode {
+        RenderMode::Forward => RenderMode::ForwardPrepass,
+        RenderMode::ForwardPrepass => RenderMode::Deferred,
+        RenderMode::Deferred => RenderMode::Forward,
+    };
+
+    println!("Switching from {:?} to {:?}", previous_mode, *mode);
+
+    for _ in materials.iter_mut() {}
+
+    match *mode {
+        RenderMode::Forward => {
+            default_opaque_renderer_method.set_to_forward();
+            for camera in &cameras {
+                commands.entity(camera).remove::<NormalPrepass>();
+                commands.entity(camera).remove::<DepthPrepass>();
+                commands.entity(camera).remove::<MotionVectorPrepass>();
+                commands.entity(camera).remove::<DeferredPrepass>();
+            }
+        }
+        RenderMode::ForwardPrepass => {
+            default_opaque_renderer_method.set_to_forward();
+            for camera in &cameras {
+                commands.entity(camera).insert(NormalPrepass);
+                commands.entity(camera).insert(DepthPrepass);
+                commands.entity(camera).insert(MotionVectorPrepass);
+                commands.entity(camera).remove::<DeferredPrepass>();
+            }
+        }
+        RenderMode::Deferred => {
+            default_opaque_renderer_method.set_to_deferred();
+            for camera in &cameras {
+                commands.entity(camera).remove::<NormalPrepass>();
+                commands.entity(camera).insert(DepthPrepass);
+                commands.entity(camera).insert(MotionVectorPrepass);
+                commands.entity(camera).insert(DeferredPrepass);
+            }
+        }
     }
 }
