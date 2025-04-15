5859793d1dd97b4ad92fe0a254abf3595a60c3f1
Greeble <166992735+greeble-dev@users.noreply.github.com>
2025-04-15 11:25:35 +0100

    Re-export all the things. This is enough to run example `3d_scene`, but things like meshlets still needing fixing up.

--- crates/bevy_pbr/src/lib.rs
+++ crates/bevy_pbr/src/lib.rs
@@ -5,3 +5,27 @@
     html_logo_url = "https://bevyengine.org/assets/icon.png",
     html_favicon_url = "https://bevyengine.org/assets/icon.png"
 )]
+
+pub use bevy_pbr_internal::{
+    decal, irradiance_volume, light_consts, queue_material_meshes, wireframe, Atmosphere,
+    AtmosphereSettings, CascadeShadowConfigBuilder, CubemapVisibleEntities,
+    DefaultOpaqueRendererMethod, DirectionalLight, DirectionalLightShadowMap, DrawMesh,
+    ExtendedMaterial, ExtractedPointLight, FogVolume, GlobalClusterableObjectMeta, Lightmap,
+    MaterialExtension, MaterialPipeline, MaterialPipelineKey, MeshInputUniform, MeshMaterial3d,
+    MeshPipeline, MeshPipelineKey, MeshPipelineViewLayoutKey, MeshUniform, NotShadowCaster,
+    NotShadowReceiver, OpaqueRendererMethod, PbrPlugin, PointLight, PointLightShadowMap,
+    RenderMeshInstances, ScreenSpaceAmbientOcclusion, ScreenSpaceAmbientOcclusionQualityLevel,
+    ScreenSpaceReflections, SetMeshBindGroup, SetMeshViewBindGroup, ShadowFilteringMethod,
+    SpotLight, StandardMaterial, TransmittedShadowReceiver, UvChannel, VisibleMeshEntities,
+    VolumetricFog, VolumetricLight, MAX_JOINTS,
+};
+
+pub mod prelude {
+    #[doc(hidden)]
+    pub use bevy_pbr_internal::{
+        environment_map::EnvironmentMapLight, light_consts, AmbientLight, Atmosphere,
+        CascadeShadowConfigBuilder, DirectionalLight, DistanceFog, FogFalloff, LightProbe,
+        Material, MaterialPlugin, MeshMaterial3d, ParallaxMappingMethod, PointLight, SpotLight,
+        StandardMaterial,
+    };
+}
