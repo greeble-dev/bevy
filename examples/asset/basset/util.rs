03a4acd0c039e9bdfd3b018f5cebc313f8535431
Greeble <166992735+greeble-dev@users.noreply.github.com>
2026-09-26 07:00:51 +0100

    Fixed meshlet debug getting out of sync if meshes are spawned while enabled.

--- examples/asset/basset/util.rs
+++ examples/asset/basset/util.rs
@@ -14,44 +14,55 @@ impl Material for MeshletDebugMaterial {}
 pub struct StashedStandardMaterial(pub Handle<StandardMaterial>);
 
 #[derive(Resource)]
-pub struct MeshletDebugMaterialHandle(pub Handle<MeshletDebugMaterial>);
+pub struct MeshletDebug {
+    pub material: Handle<MeshletDebugMaterial>,
+    enabled: bool,
+}
 
-pub fn toggle_meshlet_debug(
+impl MeshletDebug {
+    fn new(material: Handle<MeshletDebugMaterial>) -> Self {
+        Self {
+            material,
+            enabled: false,
+        }
+    }
+}
+
+pub fn toggle_meshlet_debug(mut debug: ResMut<MeshletDebug>) {
+    debug.enabled = !debug.enabled;
+}
+
+pub fn update_meshlet_debug(
     mut commands: Commands,
-    debug_material: Res<MeshletDebugMaterialHandle>,
-    query: Query<
-        (
-            Entity,
-            Option<&MeshMaterial3d<StandardMaterial>>,
-            Option<&StashedStandardMaterial>,
-        ),
-        With<MeshletMesh3d>,
-    >,
+    debug: Res<MeshletDebug>,
+    enabled: Query<(Entity, &StashedStandardMaterial), With<MeshletMesh3d>>,
+    disabled: Query<(Entity, &MeshMaterial3d<StandardMaterial>), With<MeshletMesh3d>>,
 ) {
-    for (entity, standard, stashed) in query {
-        if let Some(stashed) = stashed {
+    if debug.enabled {
+        for (entity, unstashed) in disabled {
+            commands
+                .entity(entity)
+                .remove::<MeshMaterial3d<StandardMaterial>>()
+                .insert(MeshMaterial3d(debug.material.clone()))
+                .insert(StashedStandardMaterial(unstashed.0.clone()));
+        }
+    } else {
+        for (entity, stashed) in enabled {
             commands
                 .entity(entity)
                 .remove::<MeshMaterial3d<MeshletDebugMaterial>>()
                 .remove::<StashedStandardMaterial>()
                 .insert(MeshMaterial3d(stashed.0.clone()));
         }
-
-        if let Some(standard) = standard {
-            commands
-                .entity(entity)
-                .remove::<MeshMaterial3d<StandardMaterial>>()
-                .insert(MeshMaterial3d(debug_material.0.clone()))
-                .insert(StashedStandardMaterial(standard.0.clone()));
-        }
     }
 }
 
 fn setup_meshlet_debug(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.insert_resource(MeshletDebugMaterialHandle(
+    commands.insert_resource(MeshletDebug::new(
         asset_server.add(MeshletDebugMaterial::default()),
     ));
 
+    // XXX TODO: Make this optional. Scene viewer shouldn't use it.
     commands.spawn((
         Text::new("M: Toggle meshlet debug"),
         TextFont::from_font_size(FontSize::Px(12.0)),
@@ -72,7 +83,10 @@ impl Plugin for MeshletDebugPlugin {
 
         app.add_systems(Startup, setup_meshlet_debug).add_systems(
             Update,
-            toggle_meshlet_debug.run_if(input_just_pressed(KeyCode::KeyM)),
+            (
+                toggle_meshlet_debug.run_if(input_just_pressed(KeyCode::KeyM)),
+                update_meshlet_debug,
+            ),
         );
     }
 }
